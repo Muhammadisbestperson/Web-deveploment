bfbc93dbb05ac9e261180a7fd63ee6a20cd34ec0
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-05-18 12:54:44 +0500

    Add files via upload

---
+++ Operators and variables.py
@@ -0,0 +1,19 @@
+name="Muhammad"
+age=10
+weight=28.63
+student=True
+print("Name:",name)
+print("Datatype of Name is",type(name))
+print("Age:",age)
+print("Datatype of age is",type(age))
+print("Student:",)
+print("Datatype of Student is",type(student))
+print("Weight:",weight)
+print("Datatype of Weight is",type(weight))
+print("n/After Type Casting...")
+age = str(age)
+print(age)
+print("Datatype of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Datatype of weight is",type(weight))
