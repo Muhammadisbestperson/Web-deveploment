d69946ac0fb69ec41b5de8302826ee3cff691f00
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-05-24 13:34:16 +0500

    Add files via upload

---
+++ Playing with Strings .py
@@ -0,0 +1,22 @@
+Name="Muhammad"
+Age=10.67
+Hobby="Coding"
+Grade=5
+Weight=26.91
+print("Name:",Name)
+print("Datatype of Name is",type(Name))
+print("Age:",Age)
+print("Datatype of Age is",type(Age))
+print("Hobby:",Hobby)
+print("Datatype of Hobby is",type(Hobby))
+print("Grade:",Grade)
+print("Datatype of Grade is",type(Grade))
+print("Weight:",Weight)
+print("Datatype of Weight is",type(Weight))
+print("n/After Type Casting...")
+age = str(Age)
+print(age)
+print("Datatype of Age is",type(Age))
+weight=int(Weight)
+print(weight)
+print("Datatype of Weight is",type(Weight))
