0c58572d2f24140102c3dfa21000c7004f4ee237
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-06-01 12:48:07 +0500

    Add files via upload

---
+++ Penguin.py
@@ -0,0 +1,3 @@
+def greet(name):
+    print("Hello,"+ name+ "good morning!",)
+greet('Muhammmad')
---
+++ valueabsolute.py
@@ -0,0 +1,7 @@
+def Absolute_number(number):
+    if number >= 0:
+        return number
+    else:
+        return -number
+print("Absolute_value of 89:",Absolute_number(89))
+print("Absolute_value of -189:",Absolute_number(-189))
