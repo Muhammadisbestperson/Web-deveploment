545656d83860207dfe7f04e82fa8832543edffc6
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-06-03 18:34:06 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,4 @@
+def Addition_Subtraction():
+    A=int(input("_"))
+    B=int(input("_"))
+print("You can add and subtract and multiply also divide numbers and write your equation in the input box." )    
