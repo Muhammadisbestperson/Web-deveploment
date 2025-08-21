f45f1e92941e1660bd69dd01e72132192d5ab32a
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-08-21 19:06:02 +0500

    Add files via upload

---
+++ playwithconsole.js
@@ -0,0 +1,10 @@
+let name="Muhammad";
+console.log(name);
+let car='Buggati'
+console.log(car)
+let weight=30.677
+console.log(weight )
+let house= "one story"
+console.log(house)
+let favcolor="Cyan"
+console.log(favcolor)
