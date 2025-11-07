6b4746016408a8c1972675402aad5b3ecaa2b79b
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-11-07 16:55:22 +0500

    Add files via upload

---
+++ MBH23.py
@@ -0,0 +1,8 @@
+setA={1,2,3,4,5}
+setB=[3,4,5,6]
+print("symettric difference in the sets said:")
+print(setA)
+print(setB)
+print("\nsymmetric differences of two sets")
+setC=setA.symmetric_difference(setB)
+print(setC)
