1e1b361da11cef13e859f93722d0340f553d6fcc
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-11-16 12:32:46 +0500

    Add files via upload

---
+++ MBH32.py
@@ -0,0 +1,11 @@
+class Parrot:
+    species="bird"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+blu=Parrot("Blu",100)
+woo=Parrot("Woo,",20)
+print("Blu is a {}".format(blu.species))
+print("Woo is a {}".format(woo.species))
+print("{} is {} years old".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
