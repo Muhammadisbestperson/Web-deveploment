484fb49071c85a020e12cea0d110449d7ffe7309
Muhammadisbestperson <muhammadbinhassan93@gmail.com>
2025-12-14 12:32:12 +0500

    Add files via upload

---
+++ MBH59.py
@@ -0,0 +1,9 @@
+def Ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when is ",n,"Iterations=",iteration)
+Ontime=(10)
+Ontime=(20)
+Ontime=(42)
+print("\nWith every 'n' the time taken and iterations will increase ")
